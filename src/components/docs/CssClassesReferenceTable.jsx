c125b40006d5644b09d9b4ec55673ca1d899f9ef
Mustapha-Nkhili <mustapha.developer.23@gmail.com>
2024-06-19 22:21:08 +0000

    fix: add horizontal overflow handling to CssClassesReferenceTable

--- src/components/docs/CssClassesReferenceTable.jsx
+++ src/components/docs/CssClassesReferenceTable.jsx
@@ -1,25 +1,30 @@
 const CssClassesReferenceTable = ({ cssClassesReference }) => {
   return (
-    <table className="w-full border-collapse mt-7">
-      <thead className="text-left bg-primary">
-        <tr>
-          <th className="p-3">class</th>
-          <th className="p-3">{cssClassesReference.className}</th>
-        </tr>
-      </thead>
-      <tbody>
-        {cssClassesReference.tableRows.map(({ cssClass, data }, index) => (
-          <tr key={index + cssClass} className="text-light-periwinkle w-full">
-            <td className="p-3 text-sm border-b border-b-border-clr border-b-solid lg-min-w-52">
-              {cssClass}
-            </td>
-            <td className="p-3 text-sm border-b border-b-border-clr border-b-solid text-gray lg-min-w-80">
-              {data}
-            </td>
+    <div className="overflow-x-auto">
+      <table className="w-full max-w-full border-collapse mt-7">
+        <thead className="text-left bg-primary">
+          <tr>
+            <th className="p-3">class</th>
+            <th className="p-3">{cssClassesReference.className}</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {cssClassesReference.tableRows.map(({ cssClass, data }, index) => (
+            <tr
+              key={index + cssClass}
+              className="text-light-periwinkle text-nowrap"
+            >
+              <td className="p-3 text-sm border-b border-b-border-clr border-b-solid lg-min-w-52">
+                {cssClass}
+              </td>
+              <td className="p-3 text-sm border-b border-b-border-clr border-b-solid text-gray lg-min-w-80">
+                {data}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
