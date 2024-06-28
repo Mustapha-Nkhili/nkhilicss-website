40a666c3e49d9d0d628d7e7f6a876962f89d61b7
Mustapha-Nkhili <mustapha.developer.23@gmail.com>
2024-06-28 23:53:13 +0000

    Refactor `Footer` component to simplify state handling

--- src/components/docs/Footer.jsx
+++ src/components/docs/Footer.jsx
@@ -4,33 +4,21 @@ import {
   faHeart,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Footer = ({ prevPage, nextPage }) => {
-  const [prevPageLink, setPrevPageLink] = useState(prevPage);
-  const [nextPageLink, setNextPageLink] = useState(nextPage);
-
-  useEffect(() => {
-    if (prevPageLink && prevPageLink.includes(" ")) {
-      setPrevPageLink((prev) => prev.split(" ").join("-"));
-      console.log(prevPageLink);
-    }
-
-    if (nextPageLink && nextPageLink.includes(" ")) {
-      setNextPageLink((prev) => prev.split(" ").join("-"));
-    }
-  }, [prevPageLink, nextPageLink]);
-
-
   return (
     <footer className="text-sm text-light-periwinkle  mt-20">
       <div
-        className={`${prevPage ? "flex justify-between items-center" : "grid"} mb-10`}
+        className={`${
+          prevPage ? "flex justify-between items-center" : "grid"
+        } mb-10`}
       >
         {prevPage && (
           <Link
-            to={`/docs/${prevPageLink}`}
+            to={`/docs/${
+              prevPage.includes(" ") ? prevPage.split(" ").join("-") : prevPage
+            }`}
             className="rounded bg-light-periwinkle pt-2 pb-2 pl-5 pr-5 text-black mb-5 font-semibold capitalize"
           >
             <FontAwesomeIcon icon={faArrowLeft} className="mr-2" /> {prevPage}
@@ -39,7 +27,9 @@ const Footer = ({ prevPage, nextPage }) => {
 
         {nextPage && (
           <Link
-            to={`/docs/${nextPageLink}`}
+            to={`/docs/${
+              nextPage.includes(" ") ? nextPage.split(" ").join("-") : nextPage
+            }`}
             className={`rounded bg-light-periwinkle pt-2 pb-2 pl-5 pr-5 text-black mb-5 font-semibold capitalize ${
               !prevPage && "self-end"
             }`}
