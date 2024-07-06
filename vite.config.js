27ea744ceaadf2e478f39132728116989d6da49f
Mustapha-Nkhili <mustapha.developer.23@gmail.com>
2024-07-06 21:20:09 +0000

    Remove PurgeCSS plugin from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import pluginPurgeCss from "@mojojoejo/vite-plugin-purgecss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), pluginPurgeCss()],
+  plugins: [react()],
 });
