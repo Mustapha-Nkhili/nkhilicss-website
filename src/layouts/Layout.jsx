3ccc74258e53c26e961ef0c0e4c6c7f38b58112f
Mustapha-Nkhili <mustapha.developer.23@gmail.com>
2024-07-21 14:46:27 +0000

    Add dynamic page title generation to Layout component

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,7 +1,29 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
+import { useEffect } from "react";
 
 function Layout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const generatePageTitle = () => {
+      const title = location.pathname.split("/").pop();
+
+      if (title) {
+        const formattedTitle = title.split("-").join(" ");
+        
+        return `${
+          formattedTitle.charAt(0).toUpperCase() + formattedTitle.slice(1)
+        } - Nkhili CSS`;
+      }
+
+      return `Nkhili CSS - Build fast and responsive sites`;
+    };
+
+    const titleElement = document.getElementsByTagName("title")[0];
+    titleElement.innerText = generatePageTitle();
+  }, [location.pathname]);
+
   return (
     <>
       <Header />
